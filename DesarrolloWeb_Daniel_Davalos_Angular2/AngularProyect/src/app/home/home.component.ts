fe099264404cf3fc251a5ea0896469ecc25565fc
deriel3 <davalosdaniel42@gmail.com>
2018-12-06 13:39:14 -0500

    Update home.component.ts

--- DesarrolloWeb_Daniel_Davalos_Angular2/AngularProyect/src/app/home/home.component.ts
+++ DesarrolloWeb_Daniel_Davalos_Angular2/AngularProyect/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Directive, ViewChild,AfterViewInit } from '@angular/core';
 import {LocalStorageService,SessionStorageService} from 'ngx-webstorage';
-import { Barra} from '../barra/barra.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,14 +7,6 @@ import { Barra} from '../barra/barra.component';
 })
 export class HomeComponent implements OnInit {
   constructor(private localst:LocalStorageService) { }
-  @ViewChild('child1') childOne: Barra;
   ngOnInit() {
-    console.log(this.childOne.compras);
-  }
-  ngOnChanges() {
-    if(this.localst.retrieve("excompra")!=null)
-    {
-      this.childOne.compras="true";
-    }
   }
 }
